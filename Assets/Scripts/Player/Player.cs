88bd68b6945c578d0c6b6732139533f127f3f429
duru100470 <zenpia@naver.com>
2024-01-26 23:10:30 +0900

    Player 추상클래스 구현

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,13 +1,24 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
-public class Player : MonoBehaviour
+public abstract class Player : MonoBehaviour
 {
     private ReactiveProperty<int> _health = new();
     private ReactiveProperty<int> _ammo = new();
     private ReactiveProperty<int> _maxAmmo = new();
     private ReactiveProperty<PlayerActionType> _actionType = new();
+
+    public ReactiveProperty<int> Health => _health;
+    public ReactiveProperty<int> Ammo => _ammo;
+    public ReactiveProperty<int> MaxAmmo => _maxAmmo;
+    public ReactiveProperty<PlayerActionType> ActionType => _actionType;
+
+    public float HitChance => _ammo.Value / _maxAmmo.Value;
+
+    public abstract void UseUltimate();
 }
 
 public enum PlayerActionType
