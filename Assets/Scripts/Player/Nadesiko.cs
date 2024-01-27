111b03f92b228835730ab13c5f8f568c2f5a8d8b
duru100470 <zenpia@naver.com>
2024-01-27 12:31:56 +0900

    나씨 스텟 변경

--- Assets/Scripts/Player/Nadesiko.cs
+++ Assets/Scripts/Player/Nadesiko.cs
@@ -4,8 +4,8 @@ public class Nadesiko : Player
 {
     public Nadesiko()
     {
-        _maxHealth = 2;
-        _health = 2;
+        _maxHealth = 1;
+        _health = 1;
         _maxAmmo = 5;
         _ammo = 1;
         _playerType = PlayerType.D;
@@ -17,7 +17,7 @@ public class Nadesiko : Player
         {
             var rand = new Random();
 
-            if (rand.Next(0, 2) == 0)
+            if (rand.Next(0, 10) < 2)
                 _health = Math.Clamp(_health - 1, 0, _maxHealth);
             else
                 IsUltimateUsed = true;
